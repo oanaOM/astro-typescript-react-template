c50d000938dc541a12ea769eedbf5ccb3b2745a7
OM <oana@oms-macbook-pro.home>
2023-12-23 18:22:23 +0000

    fix: error about className invalid propery

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -4,6 +4,6 @@ interface GreetingsProps {
 }
 
 export default function Greetings({ name }: GreetingsProps) {
-  return <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white-900 md:text-5xl lg:text-6xl dark:text-white">{name}</h1>
+  return <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white-900 md:text-5xl lg:text-6xl dark:text-white">{name}</h1>
     ;
 }
